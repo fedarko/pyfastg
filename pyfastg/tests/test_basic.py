c523654aec8b784bdde6d92962ae30d250e913d5
Marcus Fedarko <mfedarko@ucsd.edu>
2019-09-30 18:16:17 -0700

    TST: add another (more manageable) test graph

--- pyfastg/tests/test_basic.py
+++ pyfastg/tests/test_basic.py
@@ -1,7 +1,7 @@
 from pyfastg import parse_fastg
 
 
-def test_parse_simple_assembly_graph():
+def test_parse_medium_assembly_graph():
     digraph = parse_fastg("pyfastg/tests/input/assembly_graph.fastg")
     assert len(digraph.nodes) == 66
     assert len(digraph.edges) == 86
@@ -9,3 +9,25 @@ def test_parse_simple_assembly_graph():
         si = str(i)
         assert si + "+" in digraph.nodes
         assert si + "-" in digraph.nodes
+
+
+def test_parse_small_assembly_graph():
+    digraph = parse_fastg("pyfastg/tests/input/small.fastg")
+    assert len(digraph.nodes) == 6
+    assert len(digraph.edges) == 8
+    for i in range(1, 3):
+        si = str(i)
+        assert si + "+" in digraph.nodes
+        assert si + "-" in digraph.nodes
+    valid_edges = (
+        ("2+", "1+"),
+        ("2+", "3-"),
+        ("2+", "3+"),
+        ("1+", "3-"),
+        ("3-", "2-"),
+        ("3+", "1-"),
+        ("3+", "2-"),
+        ("1-", "2-"),
+    )
+    for e in valid_edges:
+        assert e in digraph.edges
