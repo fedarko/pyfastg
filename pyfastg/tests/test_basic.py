997fafe23c545183517d6d7db9bfd7f6c17e689d
Marcus Fedarko <mfedarko@ucsd.edu>
2019-09-30 18:28:12 -0700

    TST: test attrs more closely

--- pyfastg/tests/test_basic.py
+++ pyfastg/tests/test_basic.py
@@ -15,10 +15,16 @@ def test_parse_small_assembly_graph():
     digraph = parse_fastg("pyfastg/tests/input/small.fastg")
     assert len(digraph.nodes) == 6
     assert len(digraph.edges) == 8
-    for i in range(1, 3):
+    i2length = {1: 9, 2: 3, 3: 5}
+    i2cov = {1: 4.5, 2: 100, 3: 16.5}
+    for i in range(1, 4):
         si = str(i)
-        assert si + "+" in digraph.nodes
-        assert si + "-" in digraph.nodes
+        for suffix in ("+", "-"):
+            name = si + suffix
+            assert name in digraph.nodes
+            assert digraph.nodes[name]["coverage"] == i2cov[i]
+            assert digraph.nodes[name]["length"] == i2length[i]
+
     valid_edges = (
         ("2+", "1+"),
         ("2+", "3-"),
