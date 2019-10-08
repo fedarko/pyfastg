edcdec4c0628cab65d319c6590c252ed9f143eb8
Marcus Fedarko <mfedarko@ucsd.edu>
2019-10-07 18:12:22 -0700

    TST: test failure case in node info extraction

--- pyfastg/tests/test_extract_node_attrs.py
+++ pyfastg/tests/test_extract_node_attrs.py
@@ -1,3 +1,4 @@
+import pytest
 from ..pyfastg import extract_node_attrs
 
 
@@ -15,3 +16,13 @@ def test_alphabetical_ids():
 
     attrs = extract_node_attrs("EDGE_lmao_length_1_cov_3")
     assert attrs == {"name": "lmao+", "length": 1, "cov": 3}
+
+
+def test_bad_declaration():
+    with pytest.raises(ValueError) as exc_info:
+        extract_node_attrs("EDGE_asdf_length_100_cog_29.087'")
+    assert "Wasn't able to find all info" in str(exc_info.value)
+
+    with pytest.raises(ValueError) as exc_info:
+        extract_node_attrs("")
+    assert "Wasn't able to find all info" in str(exc_info.value)
