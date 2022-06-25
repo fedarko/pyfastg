b0974ed5d4f6b290a475a91558dab6137a3573af
Marcus Fedarko <mfedarko@ucsd.edu>
2022-06-24 22:23:11 -0700

    TST: more paranoid testing

--- pyfastg/tests/test_parse.py
+++ pyfastg/tests/test_parse.py
@@ -83,18 +83,20 @@ def test_parse_stuff_at_start_assembly_graph():
 
 
 def test_parse_no_rc_assembly_graph():
-    """Tests a graph where A+ -> C-, but not C+ -> A-, exists.
+    """Tests a graph where 1+ -> 3-, but not 3+ -> 1-, exists.
 
     Ensures that this second edge isn't automatically created.
 
-    Also, this graph has a case where B+ exists but not but B-. This is also
-    ok.
+    Also, this graph has a case where 2+ exists but not but 2-,
+    and where 4- exists but not 4+. Both these cases are ok, and
+    we verify that these reverse-complement nodes are also not automatically
+    created.
     """
-    digraph = parse_fastg("pyfastg/tests/input/small_norc.fastg")
-    assert len(digraph.nodes) == 5
+    digraph = parse_fastg("pyfastg/tests/input/norc.fastg")
+    assert len(digraph.nodes) == 6
     assert len(digraph.edges) == 2
 
-    assert set(digraph.nodes) == {"1+", "1-", "2+", "3+", "3-"}
+    assert set(digraph.nodes) == {"1+", "1-", "2+", "3+", "3-", "4-"}
     assert ("1+", "3-") in digraph.edges
     assert ("1-", "2+") in digraph.edges
 
@@ -107,4 +109,17 @@ def test_parse_no_seq_assembly_graph():
     """
     with pytest.raises(ValueError) as exc_info:
         parse_fastg("pyfastg/tests/input/noseq.fastg")
-    assert str(exc_info.value) == "length not present for all edges" 
+    assert str(exc_info.value) == "length not present for all edges"
+
+
+def test_parse_bad_len_assembly_graph():
+    """Tests a graph where the sequence length != the declared length.
+
+    This is kind of like an integration test that verifies that the
+    call to check_all_attrs_present() after creating the graph is good.
+    """
+    with pytest.raises(ValueError) as exc_info:
+        parse_fastg("pyfastg/tests/input/bad_len.fastg")
+    assert str(exc_info.value) == (
+        "Length given vs. actual seq. length differs for edge 1-"
+    )
