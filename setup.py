01dc11bc48f5c2ca10ecd18cb928cd6957c6bc2f
Marcus Fedarko <mfedarko@ucsd.edu>
2022-06-27 02:43:51 -0700

    REL: bump version to 0.1.0-dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md") as f:
 
 setup(
     name="pyfastg",
-    version="0.1.0",
+    version="0.1.0-dev",
     license="MIT",
     description=description,
     long_description=long_description,
